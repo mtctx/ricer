8002fc7ff1c93404eee0e7f45767b68ad6194369
mtctx <me@mtctx.dev>
2025-12-22 15:10:02 +0100

    Update install script

--- ricer.sh
+++ ricer.sh
@@ -274,7 +274,9 @@ kvantummanager --set catppuccin-mocha-mauve
 # Konsole
 sudo mkdir -p $HOME/.local/share/konsole/
 curl -Lo konsole-catppuccin-mocha.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-mocha.colorscheme
+sudo rm -rf $HOME/.local/share/konsole/catppuccin-mocha.colorscheme
 ln -sf "$rice_directory/konsole-catppuccin-mocha.colorscheme" $HOME/.local/share/konsole/catppuccin-mocha.colorscheme
+sudo rm -rf $HOME/.local/share/konsole/fish.profile
 ln -sf "$rice_directory/konsole-fish.profile" $HOME/.local/share/konsole/fish.profile
 kwriteconfig6 --file konsolerc --group "Desktop Entry" --key DefaultProfile "fish.profile"
 
